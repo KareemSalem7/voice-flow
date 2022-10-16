97e868f046bc3564d0a08a93f02618e2eee56526
MartinCalcaterra <95890232+MartinCalcaterra@users.noreply.github.com>
2022-10-16 10:47:59 -0400

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     `/time`, // BACKTICKS ARE SUPER IMPORTANT IN THIS FILE. NOT SINGLE QUOTES.
     createProxyMiddleware({
       target: `http://127.0.0.1:5000`,
-      changeOrigin: true,
+      changeOrigin: true
     })
   );
 }
