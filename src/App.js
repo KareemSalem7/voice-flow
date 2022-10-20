2a6948ea943e24113cd965f30436099b6c13019b
Kareem Salem <kareem.salem2003@gmail.com>
2022-10-19 23:54:28 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import './App.css';
+
+function clickMe(){ 
+  var jsonData = {
+    "users": [
+        {
+            "intent": "Kareem", 
+            "Content": "A cool intent and prompt"
+        }
+    ]
+  }
+  alert('click 1 detected')
+  // Send data to the backend via POST
+  fetch('http://localhost:8080/api/v1/transcripts/create', {  // Enter your IP address here
+    method: 'POST',  
+    body: JSON.stringify(jsonData) // body data type must match "Content-Type" header
+  })
+  console.log(jsonData);
+}
+
+function clickMeTwo(){
+  var jsonData = {
+    "users": [
+        {
+            "name": "Martin", 
+            "age": 19,
+            "username": "ToastMan"
+        }
+    ]
+  }
+  alert('click 2 detected')
+  // Send data to the backend via POST
+  fetch(`http://localhost:5040/`, {  // Enter your IP address here
+    method: 'POST',  
+    body: JSON.stringify(jsonData) // body data type must match "Content-Type" header
+  })
+  console.log(jsonData);
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Suggested Prompt A: Some prompt      
+          <button className="btn" onClick={clickMe}>Prompt 1</button>
+          <br></br>
+          Suggested Prompt B: Some other prompt
+        <button className="btn" onClick={clickMeTwo}>Prompt 2</button>
+        </p>
+        
+      </header>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PostList extends Component {
+    render() {
+        return(
+        <div>
+        List of posts
+        </div>
+        )
+    }
+}
+
+export default PostList
